8646e6b7440657dbc1d42703cf2cc407d15da99e
JIRAWAT WONGWICHAI <jirawat_wongwichai@cmu.ac.th>
2018-09-18 17:50:44 +0700

    success

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -3,15 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace CPE200Lab1
 {
     public class RPNCalculatorEngine :CalculatorEngine
-    {
+    {        
         public string Process(string str)
         {
+            Stack<string>  myStack = new Stack<string>();
+            string first, second, sum;
+            string[] parts = str.Split(' ');
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (isNumber(parts[i]))
+                {
+                    myStack.Push(parts[i]);
+                }
+                else if (isOperator(parts[i]) && myStack.Count >= 2)
+                {
+                    second = myStack.Pop();
+                    first = myStack.Pop();
+                    sum = calculate(parts[i], first, second);
+                    myStack.Push(sum);
+                }
+                else return "E";
+            }
+
+            if (myStack.Count == 1)
+            {
+                return myStack.Pop();
+            }
+            else return "E";
+
+
             // your code here
-            return "E";
-        }
+            
+        }     
     }
 }
